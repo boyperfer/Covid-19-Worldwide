fcbc1a7fc82274364b3d76b0b3da5d6b034d6a4d
Hoang Nguyen <boyperfer@Hoangs-MBP.lan>
2022-02-13 22:27:46 -0600

    Adjusting size of circles corresponding to the confirmed cases

--- client/src/component/circle/circle.component.jsx
+++ client/src/component/circle/circle.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Feature, Marker, Layer } from "react-mapbox-gl";
 import { useDispatch, useSelector } from "react-redux";
 
-import { selectDataCorona } from "../../redux/data/data.selectors";
+import { selectDataCorona, selectTotalCases } from "../../redux/data/data.selectors";
 import {
     popupToggleClick,
     popupToggleHover,
@@ -16,26 +16,75 @@ import coronaIcon from "../../assets/icon/icon_corona.png";
 
 const Circle = () => {
     const dataCorona = useSelector(selectDataCorona);
-
+    const totalCases = useSelector(selectTotalCases);
+    console.log(totalCases);
     const dispatch = useDispatch();
     // device detection
     const mobileCheck = mobileAndTabletcheck();
     return dataCorona.map((country, i) => {
         const { name, coordinates, confirmed } = country;
+        const sizeCircle = confirmed / totalCases * 100;
+        console.log(name, sizeCircle);
+        // const paint = {
+        //     "circle-color": "#2a1353",
+        //     "circle-opacity": 0.3,
+        //     "circle-radius":
+        //         confirmed > 0 && confirmed <= 1000
+        //             ? 5
+        //             : confirmed > 1000 && confirmed <= 10000
+        //             ? 10
+        //             : confirmed > 10000 && confirmed <= 50000
+        //             ? 15
+        //             : confirmed > 50000 && confirmed <= 100000
+        //             ? 20
+        //             : 4,
+        // };
         const paint = {
             "circle-color": "#2a1353",
-            "circle-opacity": 0.3,
+            "circle-opacity": 
+                sizeCircle > 0 && sizeCircle <= 0.00001
+                    ? 0.0625
+                    : sizeCircle > 0.00001 && sizeCircle <= 0.0001
+                    ? 0.125
+                    : sizeCircle > 0.0001 && sizeCircle <= 0.001
+                    ? 0.1875
+                    : sizeCircle > 0.001 && sizeCircle <= 0.01
+                    ? 0.25
+                    : sizeCircle > 0.01 && sizeCircle <= 0.1
+                    ? 0.3125
+                    : sizeCircle > 0.1 && sizeCircle <= 1
+                    ? 0.375
+                    : sizeCircle > 1 && sizeCircle <= 10
+                    ? 0.4375
+                    : sizeCircle > 10 && sizeCircle <= 20
+                    ? 0.5
+                    : 0.6,
             "circle-radius":
-                confirmed > 0 && confirmed <= 1000
-                    ? 5
-                    : confirmed > 1000 && confirmed <= 10000
-                    ? 10
-                    : confirmed > 10000 && confirmed <= 50000
-                    ? 15
-                    : confirmed > 50000 && confirmed <= 100000
-                    ? 20
-                    : 30,
-        };
+                sizeCircle > 0 && sizeCircle <= 0.00001
+                    ? 2
+                    : sizeCircle > 0.00001 && sizeCircle <= 0.0001
+                    ? 4
+                    : sizeCircle > 0.0001 && sizeCircle <= 0.001
+                    ? 6
+                    : sizeCircle > 0.001 && sizeCircle <= 0.01
+                    ? 8
+                    : sizeCircle > 0.01 && sizeCircle <= 0.05
+                    ? 13
+                    : sizeCircle > 0.05 && sizeCircle <= 0.1
+                    ? 18
+                    : sizeCircle > 0.1 && sizeCircle <= 0.25
+                    ? 23
+                    : sizeCircle > 0.25 && sizeCircle <= 0.5
+                    ? 28
+                    : sizeCircle > 0.5 && sizeCircle <= 1
+                    ? 32
+                    : sizeCircle > 1 && sizeCircle <= 3
+                    ? 35
+                    : sizeCircle > 3 && sizeCircle <= 15
+                    ? 40
+                    : 60
+        }
+
         return name.toLowerCase() === "china" ? (
             mobileCheck ? (
                 <Marker
--- server.js
+++ server.js
@@ -1,28 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const enforce = require('express-sslify');
-const path = require('path');
-const compression = require('compression');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const enforce = require("express-sslify");
+const path = require("path");
+const compression = require("compression");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(
+	enforce.HTTPS({
+		trustProtoHeader: true,
+	})
+);
 app.use(cors());
 app.use(compression());
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
+if (
+	process.env.NODE_ENV === "production"
+) {
+	app.use(
+		express.static(
+			path.join(
+				__dirname,
+				"client/build"
+			)
+		)
+	);
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.join(
+				__dirname,
+				"client/build",
+				"index.html"
+			)
+		);
 	});
 }
 
 app.listen(port, (error) => {
 	if (error) throw Error;
-	console.log(`'server running on port '${port}`);
+	console.log(
+		`'server running on port '${port}`
+	);
 });
